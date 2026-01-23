c9cda64600226e6e7fa223ccb7782e215460cfc9
杨标 <lovesnsfi@163.com>
2026-01-23 13:19:23 +0800

    添加api_server的地址的端口号为21114

--- src/common.rs
+++ src/common.rs
@@ -1085,7 +1085,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://www.softeem.xin".to_owned()
+    "http://www.softeem.xin:21114".to_owned()
 }
 
 #[inline]
